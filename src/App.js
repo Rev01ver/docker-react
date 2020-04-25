67ca51dfd973d593d298b565e154ae00576a49a0
Ilya <ycant@bk.ru>
2020-04-25 20:13:34 +0300

    ppdd

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there!! I was EDITED MF!
+          TI PIDORAS!
         </p>
         <a
           className="App-link"
